429ef72322fb272d3a5f4ace9ea4951e72e0678d
= <=>
2026-01-30 19:07:34 -0500

    fix MantColor

--- src/app/MantColor/[id]/page.jsx
+++ src/app/MantColor/[id]/page.jsx
@@ -4,6 +4,5 @@ import { use } from "react";
 import MantColor from "@/components/MantColor";
 
 export default function Page({ params }) {
-  const { id } = use(params);
-  return <MantColor id={id} />;
+  return <MantColor id={params.id}/>;
 }
--- src/app/MantMarcas/[id]/page.jsx
+++ src/app/MantMarcas/[id]/page.jsx
@@ -4,7 +4,5 @@ import { use } from "react";
 import MantMarcas from "@/components/MantMarcas";
 
 export default function Page({ params }) {
-  // const resolvedParams = use(params);
-  // return <MantMarcas id={resolvedParams.id} />;
   return <MantMarcas id={params.id} />;
 }
