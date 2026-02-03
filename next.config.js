278fedc069885ef6f77d31947a1142410df70a05
= <=>
2026-02-03 17:25:08 -0500

    new file

--- next.config.js
+++ next.config.js
@@ -1,21 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "res.cloudinary.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "sistemanegocio-backend-production.up.railway.app",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, 
